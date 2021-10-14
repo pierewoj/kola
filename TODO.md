d54bada847a0aff836e0d6839a7225b930acbf9b
Kuba Pierewoj <kuba@pierewoj.com>
2021-10-14 21:03:56 +0200

    Add test storage impl

--- TODO.md
+++ TODO.md
@@ -1,6 +1,3 @@
 # TODOs
-
-* Remove file IO from tests (most likely change of the logstorage API is required)
-    * https://endler.dev/2018/go-io-testing/
 * Make logstorage write implementation a bit clearer
 * Add tests for multiple reads/writes
--- pkg/kola/logstorage/logstorage_test.go
+++ pkg/kola/logstorage/logstorage_test.go
@@ -1,10 +1,14 @@
 package logstorage
 
-import "testing"
+import (
+	"errors"
+	"io"
+	"testing"
+)
 
 func TestWriteAndRead(t *testing.T) {
-	provider, _ := CreateIoPriovider("/tmp/test.txt")
-	storage, err := CreateStorage(provider)
+	provider := createTestIo()
+	storage, err := CreateStorage(&provider)
 	if err != nil {
 		t.Errorf("err=%s; want nil", err)
 		return
@@ -29,4 +33,62 @@ func TestWriteAndRead(t *testing.T) {
 	if read.LogEntry.valLen != 2 {
 		t.Errorf("read.valLen=%d; want 2", read.LogEntry.valLen)
 	}
+}
+
+type TestReader struct {
+	sip   *StubIoProvider
+	intex int
+}
+
+type TestWriter struct {
+	sip *StubIoProvider
+}
+
+func createTestIo() StubIoProvider {
+	return StubIoProvider{
+		written: make([]byte, 0),
+		buf:     make([]byte, 0),
+	}
+}
+
+func (tr *TestReader) Read(p []byte) (n int, err error) {
+	read := 0
+	for i := 0; i < len(p); i++ {
+		if tr.intex > len(tr.sip.written)-1 {
+			return i, errors.New("reached end of input")
+		}
+		p[i] = tr.sip.written[tr.intex]
+		tr.intex++
+		read += 1
+	}
+	return read, nil
+}
+
+func (tw *TestWriter) Write(p []byte) (n int, err error) {
+	tw.sip.buf = append(tw.sip.buf, p...)
+	return len(p), nil
+}
+
+type StubIoProvider struct {
+	written []byte
+	buf     []byte
+}
+
+func (sip *StubIoProvider) GetReader() (io.Reader, error) {
+	return &TestReader{
+		sip:   sip,
+		intex: 0,
+	}, nil
+}
+
+func (sip *StubIoProvider) GetWriter() (io.Writer, error) {
+	return &TestWriter{
+		sip: sip,
+	}, nil
+}
+
+func (sip *StubIoProvider) Flush() error {
+	sip.written = append(sip.written, sip.buf...)
+	sip.buf = make([]byte, 0)
+	return nil
 }
